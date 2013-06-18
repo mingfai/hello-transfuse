7b6a8560da2f7e4cee99ac3d25dcca41b71bbc1f
John Ericksen <johncarl81@gmail.com>
2013-06-17 19:10:28 -0600

    Added Event example

--- src/main/java/org/androidtransfuse/test3/TestService.java
+++ src/main/java/org/androidtransfuse/test3/TestService.java
@@ -1,11 +1,22 @@
 package org.androidtransfuse.test3;
 
+import org.androidtransfuse.event.EventManager;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.List;
 
 @Singleton public class TestService {
-    @Inject List<TestEntry> entries;
-    //service needs the adapter to do notifyDataSetChange, there could be alternative design for sure
-    @Inject TestAdapter adapter;
+    private List<TestEntry> entries;
+    private EventManager eventManager;
+
+    @Inject
+    public TestService(EventManager eventManager, List<TestEntry> entries) {
+        this.eventManager = eventManager;
+        this.entries = entries;
+    }
+
+    public void update(){
+        eventManager.trigger(new TestEvent("data"));
+    }
 }
